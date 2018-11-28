5e4c124a3d876f937917a539334e84bf1c899ef7
rbougrin <Developpez24>
2018-11-28 16:28:45 +0100

    first commit

--- src/main/java/webcart/WebCartServlet.java
+++ src/main/java/webcart/WebCartServlet.java
@@ -19,10 +19,37 @@ public class WebCartServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) 
 	throws ServletException, IOException {
-		PrintWriter out = res.getWriter();
-		out.println("hello world!");
-		
 		// TODO : print a html form using printwriter.
+		PrintWriter out = res.getWriter();
+		//out.println("<!DOCTYPE html>");
+		res.setContentType("text/html");
+		out.println("<form>");
+		out.println("<div>");
+		out.println("<form method=\"POST\" action=\"/myeshop/hello\">");
+		out.println("<table>");//start of printing
+		out.println("<tr>");//table row
+		out.println("<td>");//table data
+		out.println("<label for=\"ref\">Référence :</label>");
+		out.println("</td>");//table data
+		out.println("<td>");//table data
+		out.println("<input type=\"text\" id=\"ref\" name=\"ref\"/>");
+		out.println("</td>");//table data
+		out.println("</tr>");//table row
+		//out.println("</BR>");
+		out.println("<tr>");//table row
+		out.println("<td>");//table data
+        out.println("<label for=\"qty\">Quantity :</label>");
+		out.println("</td>");//table data
+		out.println("<td>");//table data
+        out.println("<input type=\"number\" id=\"qty\" name=\"qty\"/>");
+		out.println("</td>");//table data
+		out.println("</tr>");//table row
+		out.println("</table>");
+		out.println("</BR>");
+		out.println("<input type=\"submit\" value=\"Valider\"/>");
+		out.println("</div>");
+		out.println("</form>");
+
 	}
 
 	
